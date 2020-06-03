db1b929e6370c52ccf62c9f683e9011d29607fdd
Alex de Oliveira Venturini <alex800125@hotmail.com>
2020-06-02 23:55:34 -0300

    Database is loaded dynamically;

--- Backend/Src/Database.py
+++ Backend/Src/Database.py
@@ -1,35 +1,17 @@
+import os
 import face_recognition
 
 
 def create_database_images():
     print("Criando banco")
     list_images = []
+    folder = "../Known_Images/"
 
-    known_image_path_1 = "../Known_Images/1.jpg"
-    known_image_path_2 = "../Known_Images/2.jpg"
-    known_image_path_3 = "../Known_Images/3.jpg"
-    known_image_path_4 = "../Known_Images/4.png"
-    known_image_path_5 = "../Known_Images/5.jpg"
-
-    known_image_1 = face_recognition.load_image_file(known_image_path_1)
-    face_encoding_1 = face_recognition.face_encodings(known_image_1)[0]
-    list_images.append(face_encoding_1)
-
-    known_image_2 = face_recognition.load_image_file(known_image_path_2)
-    face_encoding_2 = face_recognition.face_encodings(known_image_2)[0]
-    list_images.append(face_encoding_2)
-
-    known_image_3 = face_recognition.load_image_file(known_image_path_3)
-    face_encoding_3 = face_recognition.face_encodings(known_image_3)[0]
-    list_images.append(face_encoding_3)
-
-    known_image_4 = face_recognition.load_image_file(known_image_path_4)
-    face_encoding_4 = face_recognition.face_encodings(known_image_4)[0]
-    list_images.append(face_encoding_4)
-
-    known_image_5 = face_recognition.load_image_file(known_image_path_5)
-    face_encoding_5 = face_recognition.face_encodings(known_image_5)[0]
-    list_images.append(face_encoding_5)
+    for filename in os.listdir(folder):
+        img = face_recognition.load_image_file(os.path.join(folder, filename))
+        if img is not None:
+            face_encoding = face_recognition.face_encodings(img)[0]
+            list_images.append(face_encoding)
 
     return list_images
 
