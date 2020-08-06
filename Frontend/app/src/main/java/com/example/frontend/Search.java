056143f0b893f8408bf11e438093e2eaee537a14
Alex de Oliveira Venturini <alex800125@hotmail.com>
2020-08-05 23:46:04 -0300

    Initial implementation of Suggestion page;

--- Frontend/app/src/main/java/com/example/frontend/Search.java
+++ Frontend/app/src/main/java/com/example/frontend/Search.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +12,25 @@ import androidx.fragment.app.Fragment;
 
 public class Search extends Fragment {
 
+    private View mView;
+
+    private TextView mName;
+    private TextView mAge;
+    private TextView mSuggestion;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.search, container, false);
+        View view = inflater.inflate(R.layout.search, container, false);
+        this.mView = view;
+
+        mName = mView.findViewById(R.id.name);
+        mName.setText("Alex");
+        mAge = mView.findViewById(R.id.age);
+        mAge.setText("23" + " anos");
+        mSuggestion = mView.findViewById(R.id.suggestion);
+        mSuggestion.setText("Camisa xadrez");
+
+        return view;
     }
 }
